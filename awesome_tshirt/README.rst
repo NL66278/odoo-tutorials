5558d113862846a4cddd9ba80babb1a59eefbfff
Ronald Portier <ronald@therp.nl>
2025-03-25 15:29:02 +0100

    [IMP] Add awesome_thsirt licence

---
+++ awesome_tshirt/README.rst
@@ -0,0 +1,9 @@
+Awesome Tshirt
+--------------
+
+    This app helps you to manage a business of printing customized t-shirts
+    for online customers. It offers a public page allowing customers to make
+    t-shirt orders.
+
+    Note that this is just a toy app intended to learn the javascript
+    framework.
--- awesome_tshirt/__manifest__.py
+++ awesome_tshirt/__manifest__.py
@@ -1,45 +1,30 @@
-# -*- coding: utf-8 -*-
 {
-    'name': "Awesome Shirt",
-
-    'summary': """
+    "name": "Awesome Shirt",
+    "summary": """
         Short (1 phrase/line) summary of the module's purpose, used as
         subtitle on modules listing or apps.openerp.com""",
-
-    'description': """
-        This app helps you to manage a business of printing customized t-shirts
-        for online customers. It offers a public page allowing customers to make
-        t-shirt orders.
-
-        Note that this is just a toy app intended to learn the javascript
-        framework.
-    """,
-
-    'author': "Odoo",
-    'website': "https://www.odoo.com/",
-
-    'category': 'Productivity',
-    'version': '0.1',
-    'application': True,
-    'installable': True,
-
-
+    "author": "Odoo",
+    "website": "https://www.odoo.com/",
+    "license": "AGPL-3",
+    "category": "Productivity",
+    "version": "16.0.0.1.0",
+    "application": True,
+    "installable": True,
     # any module necessary for this one to work correctly
-    'depends': ['base', 'web', 'mail', 'awesome_gallery'],
-
+    "depends": ["base", "web", "mail", "awesome_gallery"],
     # always loaded
-    'data': [
-        'security/ir.model.access.csv',
-        'views/views.xml',
-        'views/templates.xml',
+    "data": [
+        "security/ir.model.access.csv",
+        "views/views.xml",
+        "views/templates.xml",
     ],
     # only loaded in demonstration mode
-    'demo': [
-        'demo/demo.xml',
+    "demo": [
+        "demo/demo.xml",
     ],
-    'assets': {
-        'web.assets_backend': [
-            'awesome_tshirt/static/src/**/*',
+    "assets": {
+        "web.assets_backend": [
+            "awesome_tshirt/static/src/**/*",
         ],
-    }
+    },
 }
